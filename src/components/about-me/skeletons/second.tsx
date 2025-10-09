c347db4e783857acbd1a93ea94da4365c8805124
lautin0 <byzantium01@gmail.com>
2025-10-08 20:53:48 -0400

    refactor: skeleton copy, prettier

--- src/components/about-me/skeletons/second.tsx
+++ src/components/about-me/skeletons/second.tsx
@@ -1,9 +1,9 @@
-"use client";
+'use client';
 
-import React, { useCallback, useId, useRef, useState } from "react";
+import React, { useCallback, useId, useRef, useState } from 'react';
 
-import { SparklesCore } from "@/components/ui/sparkles";
-import { cn } from "@/lib/utils";
+import { SparklesCore } from '@/components/ui/sparkles';
+import { cn } from '@/lib/utils';
 
 export const SkeletonTwo = () => {
   const [sliderXPercent, setSliderXPercent] = useState(50);
@@ -21,7 +21,7 @@ export const SkeletonTwo = () => {
       setSliderXPercent(Math.max(0, Math.min(100, percent)));
     },
 
-    [],
+    []
   );
 
   return (
@@ -36,7 +36,7 @@ export const SkeletonTwo = () => {
         className="h-full w-px absolute top-0 m-auto z-30 bg-gradient-to-b from-transparent from-[10%] to-[90%] via-cyan-500 to-transparent"
         style={{
           left: `${sliderXPercent}%`,
-          top: "0",
+          top: '0',
         }}
       >
         <div className="w-36 h-full [mask-image:radial-gradient(100px_at_left,white,transparent)] absolute top-1/2 -translate-y-1/2 left-0 bg-gradient-to-r from-amber-400 via-transparent to-transparent z-20 opacity-50" />
@@ -105,7 +105,7 @@ export const SkeletonTwo = () => {
         <Cursor
           className="top-60 left-12 group-hover:left-32 group-hover:top-44"
           textClassName="group-hover:text-white"
-          text="Tyler Durden"
+          text="Google Tag Manager"
           style={{
             opacity: 1 - sliderXPercent * 0.015,
           }}
@@ -115,18 +115,12 @@ export const SkeletonTwo = () => {
   );
 };
 
-const Cover = ({
-  children,
-  className,
-}: {
-  children: React.ReactNode;
-  className?: string;
-}) => {
+const Cover = ({ children, className }: { children: React.ReactNode; className?: string }) => {
   return (
     <span
       className={cn(
-        "border rounded-md px-1 py-0.5 mr-1 border-amber-500 bg-amber-500/10 text-white",
-        className,
+        'border rounded-md px-1 py-0.5 mr-1 border-amber-500 bg-amber-500/10 text-white',
+        className
       )}
     >
       {children}
@@ -161,17 +155,14 @@ const Cursor = ({
   text?: string;
 }) => {
   return (
-    <div
-      className={cn("absolute h-4 w-4  transition-all duration-200", className)}
-      {...props}
-    >
+    <div className={cn('absolute h-4 w-4  transition-all duration-200', className)} {...props}>
       <svg
         width="19"
         height="19"
         viewBox="0 0 19 19"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
-        className={cn("h-4 w-4  transition duration-200", className)}
+        className={cn('h-4 w-4  transition duration-200', className)}
       >
         <path
           d="M3.08365 1.18326C2.89589 1.11581 2.70538 1.04739 2.54453 1.00558C2.39192 0.965918 2.09732 0.900171 1.78145 1.00956C1.41932 1.13497 1.13472 1.41956 1.00932 1.78169C0.899927 2.09756 0.965674 2.39216 1.00533 2.54477C1.04714 2.70562 1.11557 2.89613 1.18301 3.0839L5.9571 16.3833C6.04091 16.6168 6.12128 16.8408 6.2006 17.0133C6.26761 17.1591 6.42 17.4781 6.75133 17.6584C7.11364 17.8555 7.54987 17.8612 7.91722 17.6737C8.25317 17.5021 8.41388 17.1873 8.48469 17.0433C8.56852 16.8729 8.65474 16.6511 8.74464 16.4198L10.8936 10.8939L16.4196 8.74489C16.6509 8.655 16.8726 8.56879 17.043 8.48498C17.187 8.41416 17.5018 8.25346 17.6734 7.91751C17.8609 7.55016 17.8552 7.11392 17.6581 6.75162C17.4778 6.42029 17.1589 6.2679 17.0131 6.20089C16.8405 6.12157 16.6165 6.0412 16.383 5.9574L3.08365 1.18326Z"
@@ -184,11 +175,11 @@ const Cursor = ({
       </svg>
       <div
         className={cn(
-          "absolute top-3 left-3 p-1 rounded-md  text-[10px] whitespace-pre text-neutral-500  transition duration-200",
-          textClassName,
+          'absolute top-3 left-3 p-1 rounded-md  text-[10px] whitespace-pre text-neutral-500  transition duration-200',
+          textClassName
         )}
       >
-        {text ?? "Kibana"}
+        {text ?? 'Kibana'}
       </div>
     </div>
   );
@@ -203,16 +194,10 @@ const Container = ({
   children: React.ReactNode;
 }) => {
   return (
-    <div
-      className={cn(
-        "p-0.5 rounded-lg border border-neutral-600 w-fit",
-        className,
-      )}
-      {...props}
-    >
+    <div className={cn('p-0.5 rounded-lg border border-neutral-600 w-fit', className)} {...props}>
       <div
         className={cn(
-          "h-10  text-xs px-2 text-neutral-400  rounded-[5px] flex items-center justify-center bg-neutral-900 shadow-[0px_0px_8px_0px_rgba(248,248,248,0.25)_inset,0px_32px_24px_-16px_rgba(0,0,0,0.40)]",
+          'h-10  text-xs px-2 text-neutral-400  rounded-[5px] flex items-center justify-center bg-neutral-900 shadow-[0px_0px_8px_0px_rgba(248,248,248,0.25)_inset,0px_32px_24px_-16px_rgba(0,0,0,0.40)]'
         )}
       >
         {children}
@@ -221,16 +206,10 @@ const Container = ({
   );
 };
 
-const CircleWithLine = ({
-  className,
-  index = 0,
-}: {
-  className?: string;
-  index?: number;
-}) => {
+const CircleWithLine = ({ className, index = 0 }: { className?: string; index?: number }) => {
   const id = `circle-line-${index}`;
   return (
-    <div className={cn("flex flex-col items-center justify-center", className)}>
+    <div className={cn('flex flex-col items-center justify-center', className)}>
       <div
         className={cn(`rounded-full h-3 w-3 border border-[rgba(255,255,255,0.2)] bg-[rgba(248,248,248,0.02)]
       shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset]`)}
@@ -250,14 +229,7 @@ const CircleWithLine = ({
           strokeLinecap="round"
         />
         <defs>
-          <linearGradient
-            id={id}
-            x1="1.5"
-            y1="1"
-            x2="1.5"
-            y2="265"
-            gradientUnits="userSpaceOnUse"
-          >
+          <linearGradient id={id} x1="1.5" y1="1" x2="1.5" y2="265" gradientUnits="userSpaceOnUse">
             <stop stopColor="#F8F8F8" stopOpacity="0.05" />
             <stop offset="0.530519" stopColor="#F8F8F8" stopOpacity="0.5" />
             <stop offset="1" stopColor="#F8F8F8" stopOpacity="0.05" />
